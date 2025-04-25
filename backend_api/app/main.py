9edee2e078dfeb8ec882e487ae096caaaf88abcf
Timur Movchan <timurmov25@gmail.com>
2025-04-25 20:49:59 +0300

    routers

--- backend_api/app/main.py
+++ backend_api/app/main.py
@@ -1,10 +1,3 @@
 from fastapi import FastAPI
-
-app = FastAPI(
-    root_path='/api',
-    root_path_in_servers=True
-)
-
-@app.get('/')
-async def index():
-    return {'status': 200}
+from app_factory import get_application
+app = get_application()
