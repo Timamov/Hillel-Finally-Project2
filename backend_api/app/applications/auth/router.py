653f9b1fcfdd2dde8535d6879261e6ff080f4e76
Timur Movchan <timurmov25@gmail.com>
2025-05-16 20:29:15 +0300

    added second step

--- backend_api/app/applications/auth/router.py
+++ backend_api/app/applications/auth/router.py
@@ -5,9 +5,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from applications.auth.auth_handler import auth_handler
 from applications.database.session_dependencies import get_async_session
 
+
 router_auth = APIRouter()
 
 @router_auth.post('/login')
 async def user_login(data: OAuth2PasswordRequestForm = Depends(), session: AsyncSession = Depends(get_async_session)):
     token_pair = await auth_handler.get_login_token_pairs(data, session)
-    return
+    return token_pair
