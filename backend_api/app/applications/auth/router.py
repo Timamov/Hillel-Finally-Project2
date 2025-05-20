e58373e1f7c6a774a25aef2d0434e60335feef58
Timamov <timurmov25@gmail.com>
2025-05-20 20:37:46 +0300

    Finished everything

--- backend_api/app/applications/auth/router.py
+++ backend_api/app/applications/auth/router.py
@@ -4,8 +4,9 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from applications.auth.auth_handler import auth_handler
+from applications.auth.security import get_current_user
 from applications.database.session_dependencies import get_async_session
-
+from applications.users.models import User
 
 router_auth = APIRouter()
 
@@ -20,5 +21,5 @@ async def user_login(
 
 
 @router_auth.get("/get-my-info")
-async def get_my_info():
+async def get_my_info(user: User = Depends(get_current_user)):
     pass
