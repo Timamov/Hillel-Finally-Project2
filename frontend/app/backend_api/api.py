65d5995c7ff3d9e0d75aff01693be710d56e9b45
Timamov <timurmov25@gmail.com>
2025-06-24 20:27:13 +0300

    init

--- frontend/app/backend_api/api.py
+++ frontend/app/backend_api/api.py
@@ -7,8 +7,7 @@ async def login_user(user_email: str, password: str):
     async with httpx.AsyncClient() as client:
         response = await client.post(
             url=f'{settings.BACKEND_API}auth/login',
-            data={"username": user_email, 'password': password},
-            # headers = {'Content-Type': 'application/json'}
+            data={"username": user_email, 'password': password}
 
         )
         return response.json()
@@ -18,10 +17,10 @@ async def register_user(user_email: str, password: str, name: str):
     async with httpx.AsyncClient() as client:
         response = await client.post(
             url=f'{settings.BACKEND_API}users/create',
-            json={"name": name, 'password': password, "email": user_email}
+            json={"name": name, 'password': password, "email": user_email},
+            headers={'Content-Type': 'application/json'}
 
         )
-        print(response.json(), 88888888888888888888888)
         return response.json()
 
 
@@ -39,6 +38,17 @@ async def get_current_user_with_token(request: Request) -> dict:
     access_token = request.cookies.get('access_token')
     if not access_token:
         return {}
-    user = await get_user_info(access_token)#
+    user = await get_user_info(access_token)
     user['access_token'] = access_token
     return user
+
+
+async def get_products(q: str = ""):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            url=f'{settings.BACKEND_API}products/',
+            params={"q": q}
+
+        )
+        print(response.json(), 3333333333333333333333333)
+        return response.json()
